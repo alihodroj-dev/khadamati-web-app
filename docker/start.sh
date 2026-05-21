ac36aa59f483bdd36389b66361976bad76ca97b1
hussein barakat <hussein.barakat.313371@gmail.com>
2026-05-22 00:21:00 +0300

    fix: lowercase all view directory names for Linux case-sensitive filesystem and fix storage permissions

--- docker/start.sh
+++ docker/start.sh
@@ -3,6 +3,10 @@ set -e
 
 cd /app
 
+mkdir -p storage/logs storage/framework/sessions storage/framework/views storage/framework/cache
+chown -R www-data:www-data storage bootstrap/cache
+chmod -R 775 storage bootstrap/cache
+
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
